9fd66083a001de166138ee4d0a5e1ffabdd7b7a3
prdsfef-netizen <cepd98@gmail.com>
2021-03-05 20:21:26 -0600

    conexión a base de datos y creación de archivo docker-compose

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	log "github.com/sirupsen/logrus"
 )
+
+var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
+
+type TodoItemModel struct {
+	Id int `gorm:"primary_key"`
+	Description string
+	Completed bool
+}
 
 //Check verficar estado de api
 func Check(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +32,10 @@ func init() {
 }
 
 func main() {
+	defer db.Close()
+	db.Debug().DropTableIfExists(&TodoItemModel{})
+	db.Debug().AutoMigrate(&TodoItemModel{})
+	
 	log.Info("Starting Todolist API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/api", Check).Methods("GET")
